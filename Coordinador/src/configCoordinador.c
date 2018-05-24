749ed30f353be98f31d5f53ee467356d9004e204
francoarcu <francocuri.fc@gmail.com>
2018-05-24 01:37:15 -0300

    Bug fixes

--- Coordinador/src/configCoordinador.c
+++ Coordinador/src/configCoordinador.c
@@ -3,8 +3,8 @@
 void cargarConfig() {
 	t_config *configCoord = config_create(PATH_COORDINADOR_CONFIG);
 
-	PUERTO = config_get_string_value(configCoord, "PUERTO");
-	ALGORITMO = config_get_string_value(configCoord, "ALGORITMO");
+	PUERTO = strdup(config_get_string_value(configCoord, "PUERTO"));
+	ALGORITMO = strdup(config_get_string_value(configCoord, "ALGORITMO"));
 	CANTIDAD_ENTRADAS = config_get_int_value(configCoord,"CANTIDAD_ENTRADAS");
 	BYTES_ENTRADA = config_get_int_value(configCoord, "BYTES_ENTRADA");
 	RETARDO = config_get_int_value(configCoord, "RETARDO");
