0aa9f34a7424d25d5121b63ab1b69c849d44a5ab
ggabadian <ggabadian@est.frba.utn.edu.ar>
2018-06-07 23:41:55 -0300

    ESI.c modificado

--- ESI/src/ESI.c
+++ ESI/src/ESI.c
@@ -1,8 +1,23 @@
 #include "ESI.h"
 
+int id; //Este es el ID del ESI. Lo provee el Planificador en el handshake
+
 int main(void) {
 	puts("Iniciando ESI...");
 	cargarConfig();
+
+/*
+	1. Conectarse al planificador.
+	2. Handshake planificador (recibir ID)
+	3. Conectarse al Coordinador
+	4. Handshake Coordinador
+	5. Esperar orden de ejecucion del planificador
+	6. Parsear instruccion
+	7. Encviar al coordinador la orden de ejecucion
+	8. Recibir resultado por parte del coordinador el resultado
+	9. Transimitir resultado al p,anificador
+	10. Volver a 5.
+*/
 
 	int coordinadorSocket = connectSocket(IP_COORDINADOR, PUERTO_COORDINADOR);
 	int planificadorSocket = connectSocket(IP_PLANIFICADOR, PUERTO_PLANIFICADOR);
@@ -19,8 +34,6 @@ int main(void) {
 		printf("Conectado a Planificador. \n");
 		send(planificadorSocket, "Hola Planificador", 18, 0);
 	}
-
-	//(Pendiente) Conexion al planificador
 
 	while (enviar) {
 		fgets(message, PACKAGESIZE, stdin);
