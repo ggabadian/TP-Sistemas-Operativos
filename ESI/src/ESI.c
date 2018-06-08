f773f513c027bd500d6835c52b4cecd1ee581668
francoarcu <francocuri.fc@gmail.com>
2018-06-08 03:10:09 -0300

    Corrección de socketClient

--- ESI/src/ESI.c
+++ ESI/src/ESI.c
@@ -20,20 +20,14 @@ int main(void) {
 */
 
 	int coordinadorSocket = connectSocket(IP_COORDINADOR, PUERTO_COORDINADOR);
+	printf("Conectado a Coordinador. \n");
+	send(coordinadorSocket, ESI, 4, 0); // Le avisa que es un ESI
+
 	int planificadorSocket = connectSocket(IP_PLANIFICADOR, PUERTO_PLANIFICADOR);
+	printf("Conectado a Planificador. \n");
 
 	int enviar = 1;
 	char message[PACKAGESIZE];
-
-	if (coordinadorSocket >= 0){
-		send(coordinadorSocket, ESI, 4, 0); // Le avisa que es un ESI
-		printf("Conectado a Coordinador. Escriba 'exit' para salir\n");
-	}
-
-	if (planificadorSocket >= 0){
-		printf("Conectado a Planificador. \n");
-		send(planificadorSocket, "Hola Planificador", 18, 0);
-	}
 
 	while (enviar) {
 		fgets(message, PACKAGESIZE, stdin);
